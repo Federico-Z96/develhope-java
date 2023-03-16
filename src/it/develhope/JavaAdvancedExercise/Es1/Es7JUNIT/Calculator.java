d89bcdb5f90cbf8278679cf7355f3fe0f66f1a67
federico <zac.fede96@gmail.com>
2023-03-16 22:20:53 +0100

    Es7Calculator

---
+++ src/it/develhope/JavaAdvancedExercise/Es1/Es7JUNIT/Calculator.java
@@ -0,0 +1,24 @@
+package it.develhope.JavaAdvancedExercise.Es1.Es7JUNIT;
+
+public class Calculator {
+    public int add(int a, int b) {
+
+    return a + b;
+}
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public double power(double base, double exponent) {
+        return Math.pow( base, exponent);
+    }
+}
